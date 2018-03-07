f959ba73db3cf1cd3e0032c5e78e24c0f0ae24ed
Yuma Ohgami <yuma@ohgami.jp>
2018-03-07 21:02:28 +0900

    add comment.

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 int kern_init(struct EFI_SYSTEM_TABLE *st __attribute__ ((unused)),
 	      struct fb *_fb)
 {
+	/* フレームバッファ周りの初期化 */
 	fb_init(_fb);
 	set_fg(255, 255, 255);
 	set_bg(0, 70, 250);
@@ -16,15 +17,20 @@ int kern_init(struct EFI_SYSTEM_TABLE *st __attribute__ ((unused)),
 
 	fbcon_init();
 
+	/* CPU周りの初期化 */
 	gdt_init();
 	intr_init();
 
+	/* 周辺ICの初期化 */
 	set_intr_desc(INTR_NUM_KB, keyboard_handler);
 	unsigned char mask = intr_get_mask_master();
 	mask &= ~INTR_MASK_BIT_KB;
 	intr_set_mask_master(mask);
+
+	/* CPUの割り込み有効化 */
 	sti();
 
+	/* haltして待つ */
 	while (1)
 		x86_halt();
 
